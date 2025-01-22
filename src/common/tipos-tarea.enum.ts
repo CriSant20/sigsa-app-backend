431a77b147a6b6e7619f6c5c96ae58744b98adda
Fernandofr7 <lsanchez6604@uta.edu.ec>
2025-01-22 01:02:28 -0500

    Concrete los diferentes tipos de tareas

---
+++ src/common/tipos-tarea.enum.ts
@@ -0,0 +1,9 @@
+export enum TipoTarea {
+    INFORME = "Informe",
+    TAREA = "Tarea",
+    PROYECTO = "Proyecto",
+    INVESTIGACION = "Investigacion",
+    ENSAYO = "Ensayo",
+    ARTICULO = "Articulo",
+    CONSULTA = "Consulta"
+}
